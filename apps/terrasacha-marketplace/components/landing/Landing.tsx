28fddd7384bb80501d8ada7a2381b3af4ed1a4cf
LoOW3 <ignaciodiaznanni@gmail.com>
2024-01-15 21:02:45 -0300

    test skip [skip-cd]

--- apps/terrasacha-marketplace/components/landing/Landing.tsx
+++ apps/terrasacha-marketplace/components/landing/Landing.tsx
@@ -1,6 +1,6 @@
-import CardanoWallet from '../cardano-wallet/CardanoWallet';
-import WelcomeCard from './WelcomeCard';
-import Image from 'next/image';
+import CardanoWallet from "../cardano-wallet/CardanoWallet";
+import WelcomeCard from "./WelcomeCard";
+import Image from "next/image";
 const Login = (props: any) => {
   return (
     <div className="w-full h-screen flex justify-center items-center bg-slate-200">
@@ -9,13 +9,14 @@ const Login = (props: any) => {
         src="/images/home-page/fondo_login.avif"
         alt="landing-suan-image"
         fill
-        style={{ objectFit: 'cover', objectPosition: 'center', zIndex: '0' }}
+        style={{ objectFit: "cover", objectPosition: "center", zIndex: "0" }}
       />
       <div className="fixed top-1 w-full flex justify-end pr-4 z-10">
         <CardanoWallet />I
       </div>
       <div className="h-auto w-[90%] lg:w-[90%] 2xl:w-[80%] 3xl:w-[70%] flex justify-center z-10">
         <WelcomeCard checkingWallet={props.checkingWallet} />
+        
       </div>
     </div>
   );
