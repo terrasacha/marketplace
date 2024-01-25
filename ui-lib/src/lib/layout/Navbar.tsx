ec94196208ca8859defd401d5539a2b7cb0a294d
LoOW3 <ignaciodiaznanni@gmail.com>
2024-01-25 16:04:43 -0300

    close modal

--- ui-lib/src/lib/layout/Navbar.tsx
+++ ui-lib/src/lib/layout/Navbar.tsx
@@ -35,7 +35,7 @@ export default function Navbar() {
   };
 
   const closeModal = () => {
-    setShowModal(false);
+    setShowModal((prevState) => !prevState);
   };
 
   return (
@@ -69,19 +69,21 @@ export default function Navbar() {
             )}
             <div className="flex items-center">
               {showprofile && (
-                <button
-                  onClick={openModal}
-                  className="relative h-10 flex gap-4 items-center justify-center text-sm font-normal focus:z-10 focus:outline-none text-gray-900 dark:text-white dark:border-gray-600 rounded-lg focus:ring-2 py-8 px-4"
-                >
-                  <div className="flex flex-col">
-                    <p>My wallet</p>
-                    <p className="font-light text-xs">addr...123</p>
-                  </div>
-                  <div className="relative bg-green-800 text-white font-normal rounded-lg w-10 h-10">
-                    <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                      M
-                    </p>
-                  </div>
+                <div className="relative">
+                  <button
+                    onClick={openModal}
+                    className="h-10 flex gap-4 items-center justify-center text-sm font-normal focus:z-10 focus:outline-none text-gray-900 dark:text-white dark:border-gray-600 rounded-lg focus:ring-2 py-8 px-4"
+                  >
+                    <div className="flex flex-col">
+                      <p>My wallet</p>
+                      <p className="font-light text-xs">addr...123</p>
+                    </div>
+                    <div className="relative bg-green-800 text-white font-normal rounded-lg w-10 h-10">
+                      <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                        M
+                      </p>
+                    </div>
+                  </button>
                   {showModal && (
                     <div className="absolute bottom-[-.2rem] right-0 w-80 flex items-center justify-center z-40">
                       <div className="z-50 w-full bg-white dark:bg-gray-800 rounded-md shadow-xl absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-full overflow-hidden">
@@ -94,17 +96,33 @@ export default function Navbar() {
                           <p className="text-xs font-light text-gray-500 w-3/5  break-words text-left">
                             addraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
                           </p>
+                          <button
+                            type="button"
+                            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                            onClick={() => closeModal()}
+                          >
+                            <svg
+                              className="w-3 h-3"
+                              aria-hidden="true"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 14 14"
+                            >
+                              <path
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                              />
+                            </svg>
+                            <span className="sr-only">Close modal</span>
+                          </button>
                         </div>
-                        <button
-                          onClick={() => closeModal()}
-                          className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
-                        >
-                          Close
-                        </button>
                       </div>
                     </div>
                   )}
-                </button>
+                </div>
               )}
             </div>
           </div>
