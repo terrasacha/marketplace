2d4b2e03507b3b929e6e29a2759504fcf8e5a536
LoOW3 <ignaciodiaznanni@gmail.com>
2024-02-15 15:20:52 -0300

    just ask for accesstoken to external/internalwallet

--- ui-lib/src/lib/landing/RedirectToHome.tsx
+++ ui-lib/src/lib/landing/RedirectToHome.tsx
@@ -16,10 +16,6 @@ const optionsToDisplay : any = {
   requestToken:{
     title: 'Debes solicitar tu token de acceso',
     paragraph: 'Para ingresar al marketplace primero debes solicitar tu token de acceso. Una vez lo tengas en tu billetera, podrás ingresar al marketplace.',
-  },
-  notBalance:{
-    title: 'No tienes balance suficiente',
-    paragraph: 'Si creaste tu billetera recientemente probablemente no poseas fondos suficientes para hacerte de tu token de acceso. Tranquilo, puedes fondear tu billetera haciendo click en el siguiente botón.',
   }
 }
 const RedirectToHome = (props: RedirectToHomeProps) => {
@@ -29,7 +25,7 @@ const RedirectToHome = (props: RedirectToHomeProps) => {
     /* router.push('/home'); */
   }, []);
   return (
-    <div className="bg-white rounded-2xl w-[35rem] max-w-[35rem] 2xl:w-[38%] py-10 px-12 sm:px-20 h-auto flex flex-col justify-center">
+    <div className="bg-white rounded-2xl min-w-[40rem] max-w-[40rem] 2xl:w-[38%] py-10 px-12 sm:px-20 h-auto flex flex-col justify-center">
       <h2 className="text-3xl font-normal pb-4 flex justify-center">
        {optionsToDisplay[checkingWallet]?.title}
       </h2>
@@ -45,11 +41,6 @@ const RedirectToHome = (props: RedirectToHomeProps) => {
       {checkingWallet === 'requestToken' &&
                 <button className="relative w-full flex items-center justify-center font-normal focus:z-10 focus:outline-none text-white bg-cyan-700 border border-transparent enabled:hover:bg-cyan-800 focus:ring-cyan-300 dark:bg-cyan-600 dark:enabled:hover:bg-cyan-700 dark:focus:ring-cyan-800 rounded-lg focus:ring-2 px-8 py-2">
                   Solicitar token
-                </button>
-        }
-      {checkingWallet === 'notBalance' &&
-                <button className="relative w-full flex items-center justify-center font-normal focus:z-10 focus:outline-none text-white bg-cyan-700 border border-transparent enabled:hover:bg-cyan-800 focus:ring-cyan-300 dark:bg-cyan-600 dark:enabled:hover:bg-cyan-700 dark:focus:ring-cyan-800 rounded-lg focus:ring-2 px-8 py-2">
-                  Fondear billetera
                 </button>
         }
       {poweredby && (
