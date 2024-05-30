9f6165fda1c21f878a423cda04069a219713ae2a
Neider Smith <neider.smith1@gmail.com>
2024-05-30 01:46:43 -0500

    truora suan endpoint fix

--- apps/suan-marketplace/pages/api/validations/validateExternalWalletUser.js
+++ apps/suan-marketplace/pages/api/validations/validateExternalWalletUser.js
@@ -1,23 +1,56 @@
-import { validateUser } from "@marketplaces/data-access";
+import { validateExternalWalletUser } from '@marketplaces/data-access';
+
+function getEmailFromEmailVerification(data) {
+  const emailVerification = data.validations.find(
+    (validation) => validation.type === 'email-verification'
+  );
+  return emailVerification ? emailVerification.email : null;
+}
 
 export default async function handler(req, res) {
   try {
     if (req.method === 'POST') {
-      const userId = req.query.userId
-      const step = req.query.step
+      const userId = req.query.userId;
+      const processId = req.query.processId;
 
-      if(!userId){
-        res.status(405).json({ error: 'Id de usuario no hasido enviado' });
+      // Obtener data del usuario
+
+      const responseProcess = await fetch(
+        `https://api.identity.truora.com/v1/processes/${processId}/result`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Truora-API-Key':
+              'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiIiwiYWRkaXRpb25hbF9kYXRhIjoie30iLCJjbGllbnRfaWQiOiJUQ0k3YTQ4MmQ3OWE1OWFlOGJkY2M1OGY0ZmViNzQyOWYzOSIsImV4cCI6MzI5Mzg0NzIxNywiZ3JhbnQiOiIiLCJpYXQiOjE3MTcwNDcyMTcsImlzcyI6Imh0dHBzOi8vY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb20vdXMtZWFzdC0xX1Jib0NpRXdNZyIsImp0aSI6IjAwZTRmNmU5LTZjOWUtNGIzYS1hZjg4LTgxNmRiMjJlNmNjMCIsImtleV9uYW1lIjoidGVzdCIsImtleV90eXBlIjoiYmFja2VuZCIsInVzZXJuYW1lIjoiVENJN2E0ODJkNzlhNTlhZThiZGNjNThmNGZlYjc0MjlmMzktdGVzdCJ9.p_COkM6bGlnhsaiKzcxhcZElUTpxA_k7G3em6mdyPNk',
+          },
+        }
+      );
+      const data = await responseProcess.json();
+
+      if (data?.status) {
+        const userEmail = getEmailFromEmailVerification(data);
+
+        if (!userEmail) {
+          res.status(405).json({ error: 'Error al obtener el correo validado' });
+        }
+
+        const payload = {
+          username: userEmail,
+          role: 'investor',
+          email: userEmail,
+          walletId: userId,
+        };
+
+        const validationResponse = await validateExternalWalletUser(payload);
+        res.status(200).json(validationResponse);
+      } else {
+        res.status(405).json({ error: 'Error al consultar el proceso' });
       }
-
-      const userValidation = await validateUser(userId, step);
-
-      res.status(200).json(userValidation);
     } else {
       res.status(405).json({ error: 'Método no permitido' });
     }
   } catch (error) {
-    console.error('Error occurred updating order:', error);
     res.status(500).json({ error: 'Ha ocurrido un error' });
   }
 }
