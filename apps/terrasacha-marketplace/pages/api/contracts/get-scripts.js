bba574f9cef7bc6bfc1f8499f3bae501745166ff
Neider Smith <neider.smith1@gmail.com>
2024-08-21 02:53:23 -0500

    filter projects and scripts by market

--- apps/terrasacha-marketplace/pages/api/contracts/get-scripts.js
+++ apps/terrasacha-marketplace/pages/api/contracts/get-scripts.js
@@ -1,5 +1,5 @@
 import { getScriptsList } from '@marketplaces/data-access';
 export default async function handler(req, res) {
-  const scriptsList = await getScriptsList();
+  const scriptsList = await getScriptsList(process.env['NEXT_PUBLIC_MARKETPLACE_NAME']);
   res.status(200).json(scriptsList);
 }
