c823050096021dcde14f755e72dec3196af9eb36
Neider Smith <neider.smith1@gmail.com>
2024-08-29 23:04:13 -0500

    lucid base

--- ui-lib/src/lib/landing/WelcomeCard.tsx
+++ ui-lib/src/lib/landing/WelcomeCard.tsx
@@ -7,15 +7,18 @@ import Image from 'next/image';
 /* import { useWallet } from '@meshsdk/react'; */
 import { useRouter } from 'next/router';
 import CardanoWalletGeneric from '../cardano-wallet/CardanoWalletGenericButton';
+import { SelectWalletModal } from '../ui-lib';
 interface WelcomeCardProps {
   poweredby: boolean;
   appName: string;
   checkingWallet: any;
   handleSetCheckingWallet: any;
 }
+
 const WelcomeCard = (props: WelcomeCardProps) => {
   const { poweredby, appName, checkingWallet, handleSetCheckingWallet } = props;
   const [userData, setUserData] = useState(null) as any;
+  const [openModal, setOpenModal] = useState<string | undefined>();
   /* const { disconnect } = useWallet(); */
   const router = useRouter();
   useEffect(() => {
@@ -40,102 +43,111 @@ const WelcomeCard = (props: WelcomeCardProps) => {
       return false;
     }
   }
-  return (
-    <div className="bg-white rounded-2xl w-[40rem] max-w-[35rem] 2xl:w-[45%] py-10 px-10 sm:px-10 h-auto flex flex-col justify-center">
-      {appName === 'Terrasacha' && (
-        <div className='flex justify-center'>
-        <Image
-          src="/v2/logo.svg"
-          width={400}
-          height={80}
-          alt="Logotipo de Terrasacha"
-        />
-        </div>
-      )}
 
-<h2 className="font-jostBold text-3xl  pb-4 flex justify-center text-center mt-8"
->
-      {userData
-        ? 'Crea tu billetera o utiliza una preexistente'
-        : '¡Bienvenido a nuestro Marketplace!'}
-    </h2>
-      {userData ? (
-        <p className="font-jostRegular text-sm text-center">
-          {`El siguiente paso es crear tu billetera virtual o utilizar una que hayas creado con anterioridad (asegurese de contar con las mnemonics). 
+  return (
+    <>
+      <div className="bg-white rounded-2xl w-[40rem] max-w-[35rem] 2xl:w-[45%] py-10 px-10 sm:px-10 h-auto flex flex-col justify-center">
+        {appName === 'Terrasacha' && (
+          <div className="flex justify-center">
+            <Image
+              src="/v2/logo.svg"
+              width={400}
+              height={80}
+              alt="Logotipo de Terrasacha"
+            />
+          </div>
+        )}
+
+        <h2 className="font-jostBold text-3xl  pb-4 flex justify-center text-center mt-8">
+          {userData
+            ? 'Crea tu billetera o utiliza una preexistente'
+            : '¡Bienvenido a nuestro Marketplace!'}
+        </h2>
+        {userData ? (
+          <p className="font-jostRegular text-sm text-center">
+            {`El siguiente paso es crear tu billetera virtual o utilizar una que hayas creado con anterioridad (asegurese de contar con las mnemonics). 
         Para ello haz click en "Crear billetera" o "Recuperar billetera", allí deberás seguir los requerimientos 
         para luego poder solicitar tu token de acceso y así ingresar al Marketplace.`}
-        </p>
-      ) : (
-        <p className="text-sm pb-2 text-center font-jostRegular">
-     Para comenzar a usar la aplicación, necesitas una billetera virtual con el token de acceso
-     de nuestra organización. 
-     Puedes crear tu billetera y usuario directamente en nuestra plataforma.
-          {/* , o conectar cualquier billetera externa oficial, recomendamos la utilización de "Eternl". 
+          </p>
+        ) : (
+          <p className="text-sm pb-2 text-center font-jostRegular">
+            Para comenzar a usar la aplicación, necesitas una billetera virtual
+            con el token de acceso de nuestra organización. Puedes crear tu
+            billetera y usuario directamente en nuestra plataforma.
+            {/* , o conectar cualquier billetera externa oficial, recomendamos la utilización de "Eternl". 
           Si desea obtener más detalles sobre cómo crear una billetera en "Eternl", visite este
           <a href="https://suan-1.gitbook.io/documentacion-suan-sandbox"
               target="_blank"
               className="text-[#50A4FF]">enlace</a>. */}
-        </p>
-      )}
+          </p>
+        )}
 
-      <p
-        className={`h-16 flex justify-center items-center text-sm ${
-          checkingWallet === 'unauthorized' ? 'text-red-400' : 'text-slate-600'
-        }`}
-      >
-        {/* {checkingWallet === 'uncheck'? '': checkingWallet} */}
-        {checkingWallet === 'checking' ? 'Cargando...' : ''}
-        {checkingWallet === 'authorized'
-          ? 'Billetera autorizada. Redirigiendo...'
-          : ''}
-        {checkingWallet === 'unauthorized' ? 'Billetera no autorizada.' : ''}
-      </p>
-      {userData ? (
-        <Link href={'/generate-wallet'}>
-          <button className="font-jostBold relative w-full flex items-center justify-center font-jostBold focus:z-10 focus:outline-none text-white bg-custom-marca-boton border border-transparent enabled:hover:bg-custom-marca-boton-variante  dark:bg-cyan-600 dark:enabled:hover:bg-cyan-700  rounded-lg focus:ring-2 px-8 py-2">
-
-            Crear billetera
-          </button>
-        </Link>
-      ) : (
-        <Link href={'/auth/login'}>
-        <button className="font-jostBold relative w-full flex items-center justify-center font-jostBold focus:z-10 focus:outline-none text-white bg-custom-marca-boton border border-transparent enabled:hover:bg-custom-marca-boton-variante  dark:bg-cyan-600 dark:enabled:hover:bg-cyan-700  rounded-lg focus:ring-2 px-8 py-2">
-          Ingresar
-        </button>
-      </Link>
-      )
-      }
-      {userData && (
-        <Link href="/restore-wallet">
-      <button className="font-jostBold relative w-full flex items-center justify-center font-jostBold focus:z-10 focus:outline-none text-white bg-custom-marca-boton border border-transparent enabled:hover:bg-custom-marca-boton-variante  dark:bg-cyan-600 dark:enabled:hover:bg-cyan-700  rounded-lg focus:ring-2 px-8 py-2 mt-4">
-
-            Recuperar billetera
-          </button>
-        </Link>
-      )}
-      {userData && (
-        <button className="font-jostBold relative w-full flex items-center justify-center font-jostBold focus:z-10 focus:outline-none text-custom-marca-boton-variante2 bg-transparent enabled:hover:bg-custom-marca-boton-alterno2  dark:bg-cyan-600 dark:enabled:hover:bg-cyan-700  rounded-lg focus:ring-2 px-8 py-2 mt-4"
-
-          onClick={() => {
-            signOut().then(() => router.reload());
-          }}
+        <p
+          className={`h-16 flex justify-center items-center text-sm ${
+            checkingWallet === 'unauthorized'
+              ? 'text-red-400'
+              : 'text-slate-600'
+          }`}
         >
-          Cerrar sesión como {userData.username}
+          {/* {checkingWallet === 'uncheck'? '': checkingWallet} */}
+          {checkingWallet === 'checking' ? 'Cargando...' : ''}
+          {checkingWallet === 'authorized'
+            ? 'Billetera autorizada. Redirigiendo...'
+            : ''}
+          {checkingWallet === 'unauthorized' ? 'Billetera no autorizada.' : ''}
+        </p>
+        {userData ? (
+          <Link href={'/generate-wallet'}>
+            <button className="font-jostBold relative w-full flex items-center justify-center font-jostBold focus:z-10 focus:outline-none text-white bg-custom-marca-boton border border-transparent enabled:hover:bg-custom-marca-boton-variante  dark:bg-cyan-600 dark:enabled:hover:bg-cyan-700  rounded-lg focus:ring-2 px-8 py-2">
+              Crear billetera
+            </button>
+          </Link>
+        ) : (
+          <Link href={'/auth/login'}>
+            <button className="font-jostBold relative w-full flex items-center justify-center font-jostBold focus:z-10 focus:outline-none text-white bg-custom-marca-boton border border-transparent enabled:hover:bg-custom-marca-boton-variante  dark:bg-cyan-600 dark:enabled:hover:bg-cyan-700  rounded-lg focus:ring-2 px-8 py-2">
+              Ingresar
+            </button>
+          </Link>
+        )}
+        <button
+          className="mt-4 font-jostBold relative w-full flex items-center justify-center font-jostBold focus:z-10 focus:outline-none text-white bg-custom-marca-boton border border-transparent enabled:hover:bg-custom-marca-boton-variante  dark:bg-cyan-600 dark:enabled:hover:bg-cyan-700  rounded-lg focus:ring-2 px-8 py-2"
+          onClick={() => setOpenModal('selectWalletModal')}
+        >
+          Ingresar con billetera externa
         </button>
-      )}
-      {poweredby && (
-        <div className="flex items-center justify-center mt-4 text-xs font-jostRegular">
-          Powered by
-          <Image
-            src="/images/home-page/suan_logo.png"
-            height={10}
-            width={12}
-            className="ml-2"
-            alt={`${appName} logo`}
-          />
-        </div>
-      )}
-    </div>
+        {userData && (
+          <Link href="/restore-wallet">
+            <button className="font-jostBold relative w-full flex items-center justify-center font-jostBold focus:z-10 focus:outline-none text-white bg-custom-marca-boton border border-transparent enabled:hover:bg-custom-marca-boton-variante  dark:bg-cyan-600 dark:enabled:hover:bg-cyan-700  rounded-lg focus:ring-2 px-8 py-2 mt-4">
+              Recuperar billetera
+            </button>
+          </Link>
+        )}
+        {userData && (
+          <button
+            className="font-jostBold relative w-full flex items-center justify-center font-jostBold focus:z-10 focus:outline-none text-custom-marca-boton-variante2 bg-transparent enabled:hover:bg-custom-marca-boton-alterno2  dark:bg-cyan-600 dark:enabled:hover:bg-cyan-700  rounded-lg focus:ring-2 px-8 py-2 mt-4"
+            onClick={() => {
+              signOut().then(() => router.reload());
+            }}
+          >
+            Cerrar sesión como {userData.username}
+          </button>
+        )}
+        {poweredby && (
+          <div className="flex items-center justify-center mt-4 text-xs font-jostRegular">
+            Powered by
+            <Image
+              src="/images/home-page/suan_logo.png"
+              height={10}
+              width={12}
+              className="ml-2"
+              alt={`${appName} logo`}
+            />
+          </div>
+        )}
+      </div>
+      <SelectWalletModal openModal={openModal} setOpenModal={setOpenModal} />
+      
+    </>
   );
 };
 
--- ui-lib/src/lib/modals/SelectWalletModal.tsx
+++ ui-lib/src/lib/modals/SelectWalletModal.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import AlertMessage from '../common/AlertMessage';
 import Link from 'next/link';
+import { Lucid, Blockfrost } from 'lucid-cardano';
 
 const WALLET_ICONS: any = {
   eternl: '/images/help/eternl-wallet.png',
@@ -13,6 +14,7 @@ const WALLET_ICONS: any = {
 
 export default function SelectWalletModal({ openModal, setOpenModal }: any) {
   /* const { connected, connect, connecting, error } = useWallet(); */
+  const connecting = false;
   const [walletSelected, setWalletSelected] = useState<string | undefined>(
     undefined
   );
@@ -29,7 +31,54 @@ export default function SelectWalletModal({ openModal, setOpenModal }: any) {
     visible: false,
   });
 
-  /* const wallets = useWalletList(); */
+  const getInstalledWallet = () => {
+    if (window === undefined) return [];
+    if (window.cardano === undefined) return [];
+
+    let wallets: any = [];
+    for (const key in window.cardano) {
+      try {
+        const _wallet = window.cardano[key];
+        if (_wallet === undefined) continue;
+        if (_wallet.name === undefined) continue;
+        if (_wallet.icon === undefined) continue;
+        if (_wallet.apiVersion === undefined) continue;
+        wallets.push({
+          api: _wallet,
+          id: key,
+          name: key == 'nufiSnap' ? 'MetaMask' : _wallet.name,
+          icon: _wallet.icon,
+          version: _wallet.apiVersion,
+        });
+      } catch (e) {}
+    }
+    console.log(window.cardano);
+    console.log(wallets);
+
+    return wallets;
+  };
+
+  const connectWallet = async (walletApi: any) => {
+    const wallet = await walletApi.enable();
+    console.log('Connected to wallet:', wallet);
+
+    const lucid = await Lucid.new(
+      new Blockfrost(
+        'https://cardano-preview.blockfrost.io/api/v0',
+        process.env['NEXT_PUBLIC_blockFrostKeysPreview']
+      ),
+      'Preview'
+    );
+
+    lucid.selectWallet(wallet);
+
+    const address = await lucid.wallet.address();
+
+    console.log('address', address);
+    return wallet;
+  };
+
+  const wallets = getInstalledWallet();
   /* useEffect(() => {
     if (connected) {
       setOpenModal(undefined);
@@ -80,65 +129,70 @@ export default function SelectWalletModal({ openModal, setOpenModal }: any) {
       className="z-40"
     >
       <Modal.Header className="py-3">Conectar Billetera</Modal.Header>
-      {/* <Modal.Body className="p-6">
-        <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
-          {wallets.length > 0
-            ? 'Conecta tú billetera con alguno de nuestros proveedores disponibles.'
-            : 'No encontramos ninguna wallet instalada en tu navegador.'}
-        </p>
-        <ul className="my-4 space-y-3">
-          {wallets.map((wallet) => {
-            return (
-              <li key={wallet.name}>
-                <a
-                  onClick={() => {
-                    connect(wallet.name);
-                    setWalletSelected(wallet.name);
-                    sessionStorage.setItem('preferredWalletSuan', wallet.name);
-                  }}
-                  className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
-                >
-                  <Image
-                    src={getWalletImageByName(wallet.name)}
-                    alt={wallet.name}
-                    width={25}
-                    height={25}
-                  />
-                  <span className="flex-1 ml-3 whitespace-nowrap">
-                    {capitalizeFirstLetter(wallet.name)}
-                  </span>
-                  {connecting && walletSelected === wallet.name && (
-                    <Spinner aria-label="Default status example" />
-                  )}
-                  {showError &&
-                    !connecting &&
-                    walletSelected === wallet.name && (
-                      <span className="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-red-500 bg-red-200 rounded dark:bg-red-700 dark:text-red-400">
-                        Error
-                      </span>
+      {
+        <Modal.Body className="p-6">
+          <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
+            {wallets.length > 0
+              ? 'Conecta tú billetera con alguno de nuestros proveedores disponibles.'
+              : 'No encontramos ninguna wallet instalada en tu navegador.'}
+          </p>
+          <ul className="my-4 space-y-3">
+            {wallets.map((wallet: any) => {
+              return (
+                <li key={wallet.name}>
+                  <a
+                    onClick={() => {
+                      connectWallet(wallet.api);
+                      setWalletSelected(wallet.name);
+                      sessionStorage.setItem(
+                        'preferredWalletSuan',
+                        wallet.name
+                      );
+                    }}
+                    className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
+                  >
+                    <Image
+                      src={getWalletImageByName(wallet.name)}
+                      alt={wallet.name}
+                      width={25}
+                      height={25}
+                    />
+                    <span className="flex-1 ml-3 whitespace-nowrap">
+                      {capitalizeFirstLetter(wallet.name)}
+                    </span>
+                    {connecting && walletSelected === wallet.name && (
+                      <Spinner aria-label="Default status example" />
                     )}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-        <AlertMessage
-          type={alertMessage.type}
-          title={alertMessage.title}
-          message={alertMessage.message}
-          visible={alertMessage.visible && showError}
-        ></AlertMessage>
-        <div className="mt-2">
-          <Link
-            href="https://suan-1.gitbook.io/documentacion-suan-sandbox"
-            target='_blank'
-            className="text-xs font-semibold hover:text-blue-400"
-            onClick={() => setOpenModal(undefined)}
-          >
-            ¿Cómo creo mi billetera?
-          </Link>
-        </div>
-      </Modal.Body> */}
+                    {showError &&
+                      !connecting &&
+                      walletSelected === wallet.name && (
+                        <span className="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-red-500 bg-red-200 rounded dark:bg-red-700 dark:text-red-400">
+                          Error
+                        </span>
+                      )}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+          <AlertMessage
+            type={alertMessage.type}
+            title={alertMessage.title}
+            message={alertMessage.message}
+            visible={alertMessage.visible && showError}
+          ></AlertMessage>
+          <div className="mt-2">
+            <Link
+              href="https://suan-1.gitbook.io/documentacion-suan-sandbox"
+              target="_blank"
+              className="text-xs font-semibold hover:text-blue-400"
+              onClick={() => setOpenModal(undefined)}
+            >
+              ¿Cómo creo mi billetera?
+            </Link>
+          </div>
+        </Modal.Body>
+      }
     </Modal>
   );
 }
