b87a68adbd189830bf148c8aee1c6e8b018d8811
LAPTOP-6SJOIC03\germa <betterbeat00@gmail.com>
2025-02-04 11:05:17 -0500

    cambios en next.config.js

--- apps/terrasacha-marketplace/next.config.js
+++ apps/terrasacha-marketplace/next.config.js
@@ -2,14 +2,16 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const dotenv = require('dotenv');
+
+// Cargar las variables de entorno antes de usarlas
+dotenv.config();
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
   nx: {
-    // Set this to true if you would like to use SVGR
-    // See: https://github.com/gregberge/svgr
     svgr: false,
   },
   reactStrictMode: false,
@@ -21,15 +23,20 @@ const nextConfig = {
     return config;
   },
   images: {
-    domains: [
-      `${process.env['NEXT_PUBLIC_s3BucketName']}.s3.amazonaws.com`,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: `${process.env.NEXT_PUBLIC_s3BucketName || "default-bucket"}.s3.amazonaws.com`,
+      },
+      {
+        protocol: "https",
+        hostname: "platformd9531187bef34a10abb664f2878180ae00db6-internal.s3.amazonaws.com",
+        pathname: "/public/category-projects-images/**",
+      },
     ],
   },
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
+const plugins = [withNx];
 
 module.exports = composePlugins(...plugins)(nextConfig);
