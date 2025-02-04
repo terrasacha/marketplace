13a8515b453f0c41f380c08587e37c924c558b05
LAPTOP-6SJOIC03\germa <betterbeat00@gmail.com>
2025-02-04 10:53:22 -0500

    cambios en la carpeta de amplify

--- apps/terrasacha-marketplace/next.config.js
+++ apps/terrasacha-marketplace/next.config.js
@@ -2,17 +2,15 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
-const dotenv = require('dotenv');
-
-// Cargar variables de entorno desde `.env.local`
-dotenv.config();
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
   nx: {
-    svgr: false, // Controla si SVGR está habilitado
+    // Set this to true if you would like to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
   },
   reactStrictMode: false,
   webpack: function (config, options) {
@@ -23,23 +21,15 @@ const nextConfig = {
     return config;
   },
   images: {
-    // Uso de `remotePatterns` para Next.js 13+
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: `${process.env.NEXT_PUBLIC_s3BucketName || "default-bucket"}.s3.amazonaws.com`,
-      },
-      {
-        protocol: "https",
-        hostname: "platformd9531187bef34a10abb664f2878180ae00db6-internal.s3.amazonaws.com",
-        pathname: "/public/category-projects-images/**", // Se permite acceder a imágenes dentro de esta carpeta
-      },
+    domains: [
+      `${process.env['NEXT_PUBLIC_s3BucketName']}.s3.amazonaws.com`,
     ],
   },
 };
 
 const plugins = [
-  withNx, // Next.js con Nx
+  // Add more Next.js plugins to this list if needed.
+  withNx,
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
