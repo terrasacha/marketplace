9bc70d71d726f30c468a132d4cefbc5789fafaad
RanRodri <rancesrx@gmail.com>
2025-05-20 21:54:15 -0500

    fixed amplify carpet

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,10 +3,13 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
+    "aws_cognito_identity_pool_id": "us-east-1:faee2749-d4b6-4d40-8964-13444d73fec8",
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_UbkfjlXod",
     "aws_user_pools_web_client_id": "4aa7fsknjedin026isvj94i71a",
-    "oauth": {},
+    "oauth": {
+        "domain": "us-east-1ubkfjlxod.auth.us-east-1.amazoncognito.com"
+    },
     "aws_cognito_username_attributes": [],
     "aws_cognito_social_providers": [],
     "aws_cognito_signup_attributes": [
